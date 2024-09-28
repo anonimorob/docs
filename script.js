4329cc142562988b5fb31892f97eafa1604568e7
anonimorob <115708199+anonimorob@users.noreply.github.com>
2024-09-28 15:57:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const gif = document.querySelector(".gif");
 
 // cambia testo e gif quando schiaccio yes button
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Ci sei cascato (spero tu non lo abbia cliccato subito :) ";
+  question.innerHTML = "Good choice";
   gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
 });
 
